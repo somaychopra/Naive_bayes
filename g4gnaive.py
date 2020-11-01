6fabebe97821b876aea78dd68766177f8959f5b7
Somay Chopra <thisissomaychopra@gmail.com>
2020-11-01 21:02:13 +0530

    Update g4gnaive.py

--- g4gnaive.py
+++ g4gnaive.py
@@ -2,6 +2,7 @@
 import math 
 import random 
 import csv 
+import copy
 import sklearn
 import sklearn.decomposition
 from sklearn.decomposition import PCA
@@ -11,23 +12,52 @@ from sklearn.model_selection import KFold
 from sklearn import preprocessing
 from scipy.stats import norm
 import matplotlib.pyplot as plt
+import argparse
 
 
 epsilon = 1e-12
-# the categorical class names are changed to numberic data 
-# eg: yes and no encoded to 1 and 0
+parser = argparse.ArgumentParser(description='All arguments for the program.')
+parser.add_argument('--use_inbuilt_nb', type=None, default=False, help='To use inbuilt naive bayes function.')
+args = parser.parse_args()
+use_inbuilt_nb = args.use_inbuilt_nb
+
+
+def normalize_data(train_data, val_data, test_data) :
+	train_data_np = np.array(train_data)
+	test_data_np = np.array(test_data)
+	val_data_np = np.array(val_data)
+	train_X_np = train_data_np[:,:-1]  
+	train_Y_np = train_data_np[:,-1].reshape((train_data_np.shape[0],1))
+	test_X_np = test_data_np[:,:-1]
+	test_Y_np = test_data_np[:,-1].reshape((test_data_np.shape[0],1))
+	val_X_np = val_data_np[:,:-1]
+	val_Y_np = val_data_np[:,-1].reshape((val_data_np.shape[0],1))
+	normalizer = preprocessing.Normalizer()
+	train_X_np = normalizer.fit_transform(train_X_np)
+	test_X_np = normalizer.transform(test_X_np)
+	val_X_np = normalizer.transform(val_X_np)
+	train_data = np.concatenate((train_X_np, train_Y_np), axis=1).tolist()
+	test_data = np.concatenate((test_X_np, test_Y_np), axis=1).tolist()
+	val_data = np.concatenate((val_X_np, val_Y_np), axis=1).tolist()
+	return train_data, val_data, test_data
+
+def augment_data(mydata) :
+	mydata_np = np.array(mydata)
+	for i in range(mydata_np.shape[0]) :
+		if mydata_np[i,-1] == 1 : 
+			mydata_np = np.concatenate((mydata_np, mydata_np[i,:].reshape(1,-1)), axis=0)
+	mydata = mydata_np.tolist()
+	return mydata
+
 def plot(x, y, save_path, title):
 	# Plot dataset
     plt.figure()
     plt.plot(x[y == 0, -2], x[y == 0, -1], 'go', linewidth=2)
     plt.plot(x[y == 1, -2], x[y == 1, -1], 'bx', linewidth=2)
-
-    # Add labels and save to disk
     plt.xlabel('1st Feature')
     plt.ylabel('2nd Feature')
     plt.legend(['No death', 'Death'])
     plt.title(title)
-    # plt.show()
     plt.savefig(save_path)
     plt.close()
 
@@ -41,38 +71,29 @@ def plot_hist(x, save_path, title) :
 	plt.title(title)
 	plt.savefig(save_path)
 	plt.close()
-
-
+#Encoding the raw data
 def encode_class(mydata): 
 	classes = [] 
 	for i in range(len(mydata)): 
 		if mydata[i][-1] not in classes: 
-			classes.append(mydata[i][-1])
-	# print(len(classes)) 
+			classes.append(mydata[i][-1]) 
 	for i in range(len(classes)): 
 		for j in range(len(mydata)): 
 			if mydata[j][-1] == classes[i]:
 				mydata[j][-1] = i 
 	return mydata			 
 			
-
 # Splitting the data 
 def splitting(mydata, ratio): 
 	train_num = int(len(mydata) * ratio) 
 	train = [] 
-	# initally testset will have all the dataset 
 	test = list(mydata) 
-	while len(train) < train_num: 
-		# index generated randomly from range 0 
-		# to length of testset 
+	while len(train) < train_num:  
 		index = random.randrange(len(test)) 
-		# from testset, pop data rows and put it in train 
 		train.append(test.pop(index)) 
 	return train, test 
 
-
-# Group the data rows under each class yes or 
-# no in dictionary eg: dict[yes] and dict[no] 
+# Group the data rows under each class 1 or 0
 def groupUnderClass(mydata): 
 	dict = {} 
 	for i in range(len(mydata)): 
@@ -81,12 +102,11 @@ def groupUnderClass(mydata):
 		dict[mydata[i][-1]].append(mydata[i]) 
 	return dict
 
-
-# Calculating Mean 
+#Mean Calculator
 def mean(numbers): 
 	return sum(numbers) / float(len(numbers)) 
 
-# Calculating Standard Deviation 
+#Standard Deviation calc.
 def std_dev(numbers): 
 	avg = mean(numbers) 
 	variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1) 
@@ -94,13 +114,10 @@ def std_dev(numbers):
 
 def MeanAndStdDev(mydata): 
 	info = [(mean(attribute), std_dev(attribute)) for attribute in zip(*mydata)] 
-	# eg: list = [ [a, b, c], [m, n, o], [x, y, z]] 
-	# here mean of 1st attribute =(a + m+x), mean of 2nd attribute = (b + n+y)/3 
-	# delete summaries of last class 
 	del info[-1] 
 	return info 
 
-# find Mean and Standard Deviation under each class 
+# Find Mean and Standard Deviation under each class 
 def MeanAndStdDevForClass(mydata): 
 	info = {} 
 	dict = groupUnderClass(mydata) 
@@ -108,17 +125,11 @@ def MeanAndStdDevForClass(mydata):
 		info[classValue] = MeanAndStdDev(instances)
 	return info 
 
-
-# Calculate Gaussian Probability Density Function 
-def calculateGaussianProbability(x, mean, stdev): 
-	expo = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2) + epsilon))) 
-	return (1 / (math.sqrt(2 * math.pi) * stdev + epsilon)) * expo 
-
+# Calculate Gaussian Log Probability Density Function  
 def calculateGaussianLogProbability(data, mu, sigma) :
-	# print(2*math.pi*(sigma**2))
-	# if sigma == 0 :
-		# return 0
-	ll = math.log(2*math.pi*(sigma**2) + epsilon)/2 + ((data-mu)**2)/(2 * (sigma**2) + epsilon)
+	if sigma == 0 :
+		sigma = epsilon
+	ll = math.log(2*math.pi*(sigma**2))/2 + ((data-mu)**2)/(2 * (sigma**2) )
 	return -ll
 
 # Calculate Class Probabilities 
@@ -131,18 +142,13 @@ def calculateClassLogProbabilities(info, logBerProb, test):
 			mean, std_dev = classSummaries[i]
 			if(std_dev == 0) :
 				to_Remove_Ft_list.append(i)
-				# print("Std_Dev = 0; classValue : {}; feature number : {}".format(classValue, i))
 			x = test[i]
 			probabilities[classValue] += calculateGaussianLogProbability(x, mean, std_dev)
 		probabilities[classValue] += logBerProb[int(classValue)]
 	to_Remove_Ft_list.sort()
 	theSet= set(to_Remove_Ft_list)
 	to_Remove_Ft_list = list(theSet)
-	# print(len(to_Remove_Ft_list))
-	# exit()
-	# print(probabilities)
 	return probabilities 
-
 
 # Make prediction - highest probability is the prediction 
 def predict(info, logBerProb, test): 
@@ -154,7 +160,6 @@ def predict(info, logBerProb, test):
 			bestLabel = classValue
 	return bestLabel 
 
-
 # returns predictions for a set of examples 
 def getPredictions(info, logBerProb, test): 
 	predictions = []
@@ -163,7 +168,6 @@ def getPredictions(info, logBerProb, test):
 		predictions.append(result) 
 	return predictions 
 
-# Accuracy score 
 def accuracy_rate(test, predictions): 
 	correct = 0
 	for i in range(len(test)): 
@@ -171,81 +175,36 @@ def accuracy_rate(test, predictions):
 			correct += 1
 	return (correct / float(len(test))) * 100.0
 
-def normalize_data(train_data, test_data) :
+def apply_pca(train_data, val_data, test_data, count) :
 	train_data_np = np.array(train_data)
-	test_data_np = np.array(test_data)
-	train_X_np = train_data_np[:,:-1]  
-	train_Y_np = train_data_np[:,-1].reshape((train_data_np.shape[0],1))
-	test_X_np = test_data_np[:,:-1]
-	test_Y_np = test_data_np[:,-1].reshape((test_data_np.shape[0],1))
-	
-	normalizer = preprocessing.Normalizer()
-	train_X_np = normalizer.fit_transform(train_X_np)
-	test_X_np = normalizer.transform(test_X_np)
-	# mean = np.mean(train_X_np, axis=0)
-	# std = np.std(train_X_np, axis=0) + epsilon
-	# train_X_np = (train_X_np - mean)/std
-	# test_X_np = (test_X_np - mean)/std
-	train_data = np.concatenate((train_X_np, train_Y_np), axis=1).tolist()
-	test_data = np.concatenate((test_X_np, test_Y_np), axis=1).tolist()
-
-	return train_data, test_data
-
-def normalize_data_full_data(data) :
-	data_np = np.array(data)
-	X_np = data_np[:,:-1]  
-	Y_np = data_np[:,-1].reshape((data_np.shape[0],1))
-	
-	mean = (np.mean(X_np, axis=0))
-	std = (np.std(X_np, axis=0) + epsilon)
-	X_np -= mean
-	X_np /= std
-	data = np.concatenate((X_np, Y_np), axis=1).tolist()
-
-	return data
-
-def augment_data(mydata) :
-	mydata_np = np.array(mydata)
-	for i in range(mydata_np.shape[0]) :
-		if mydata_np[i,-1] == 1 : 
-			mydata_np = np.concatenate((mydata_np, mydata_np[i,:].reshape(1,-1)), axis=0)
-	mydata = mydata_np.tolist()
-	return mydata
-
-def apply_pca(train_data, test_data, count) :
-	train_data_np = np.array(train_data)
+	val_data_np = np.array(val_data)
 	test_data_np = np.array(test_data)
 
 	train_X_np = train_data_np[:,:-1]  
 	train_Y_np = train_data_np[:,-1].reshape((train_data_np.shape[0],1))
+	val_X_np = val_data_np[:,:-1]  
+	val_Y_np = val_data_np[:,-1].reshape((val_data_np.shape[0],1))
 	test_X_np = test_data_np[:,:-1]
 	test_Y_np = test_data_np[:,-1].reshape((test_data_np.shape[0],1))
 
-
-	# pca = PCA()
 	pca = PCA(n_components=0.95)
-	# pca = PCA(n_components=5)
 	train_X_np_pca = pca.fit_transform(train_X_np)
 	test_X_np_pca = pca.transform(test_X_np)
-	# print(test_X_np_pca.shape)
-	# print(train_X_np_pca.shape)
+	val_X_np_pca = pca.transform(val_X_np)
+	
 	plot(x=test_X_np_pca[:,:2],y=test_Y_np.reshape(-1),save_path="Projected Features {}.".format(count),title="Projected Features in the first two dimension.")
 	train_data = np.concatenate((train_X_np_pca, train_Y_np), axis=1).tolist()
 	test_data = np.concatenate((test_X_np_pca, test_Y_np), axis=1).tolist()
+	val_data = np.concatenate((val_X_np_pca, val_Y_np), axis=1).tolist()
 
-	pca_plot = PCA()
-	pca_plot.fit_transform(train_X_np)
-	# plot_hist(pca_plot.explained_variance_ratio_, save_path="PCA Graph Explained Variance Ratio", title="Explained Variance Ratio")
 	plot_hist(pca.explained_variance_ratio_, save_path="PCA Graph Explained Variance Ratio of Selected Components {}".format(count), title="Explained Variance Ratio of Selected Components")
 
-	return train_data, test_data
+	return train_data, val_data, test_data
 
 
 def berProb(train_data) :
 	train_data_np = np.array(train_data)
 	train_data_np = train_data_np[:,-1]
-	# print(train_data_np.shape[0])
-	# print(np.sum(train_data_np, axis=0))
 	p = np.sum(train_data_np, axis=0)/train_data_np.shape[0]
 	logBerProb = []
 	logBerProb.append(np.log(1-p))
@@ -277,14 +236,16 @@ def InbuiltGaussianNb(train_data, test_data) :
 	return accuracy, accuracy_train
 
 
-def k_fold_cv(train_data, pca=False, use_inbuilt_nb=False):
+def k_fold_cv(train_data=None, pca=False, use_inbuilt_nb=False, test_data=None):
 	k = 1
 	print("Performing 5 fold cross validation : ")
 	kfold = KFold(5, shuffle=True)
 	avg_accuracy = 0
 	avg_accuracy_train = 0
 	count = 0
+	test_orig = copy.deepcopy(test_data)
 	for train, valid in kfold.split(train_data):
+		test_data = copy.deepcopy(test_orig)
 		count += 1
 		i_fold_train = []
 		i_fold_valid = []
@@ -292,11 +253,9 @@ def k_fold_cv(train_data, pca=False, use_inbuilt_nb=False):
 			i_fold_train.append(train_data[i])
 		for i in valid:
 			i_fold_valid.append(train_data[i])
-		# i_fold_train = train_data[train]
-		# i_fold_valid = train_data[valid]
-		i_fold_train, i_fold_valid = normalize_data(i_fold_train, i_fold_valid)
+		i_fold_train, i_fold_valid, test_data = normalize_data(train_data=i_fold_train,val_data=i_fold_valid,test_data=test_data)
 		if pca :
-			i_fold_train, i_fold_valid = apply_pca(i_fold_train, i_fold_valid, count)
+			i_fold_train, i_fold_valid, test_data = apply_pca(train_data=i_fold_train, val_data=i_fold_valid, test_data=test_data, count=count)
 			print("Number of features selected : {}".format(len(i_fold_train[0])-1))
 
 		if use_inbuilt_nb : 
@@ -311,30 +270,126 @@ def k_fold_cv(train_data, pca=False, use_inbuilt_nb=False):
 		avg_accuracy += accuracy
 		avg_accuracy_train += accuracy_train 
 		print("Accuracy on validation set on",k,"th fold is: ",accuracy)
-		# print("Accuracy on training set on",k,"th fold is: ",accuracy_train)
-		# print(predictions)
 		k = k + 1
 	avg_accuracy /= 5
 	avg_accuracy_train /= 5
-	print("Average Accuracy on validation set on : ",avg_accuracy)
-	print("Average Accuracy on training set on : ",avg_accuracy_train)
+	print("Average Accuracy on validation set : ",avg_accuracy)
+	print("Average Accuracy on training set : ",avg_accuracy_train)
+	if not use_inbuilt_nb :
+		predictions_test = getPredictions(info, logBerProb, test_data)
+		accuracy_test = accuracy_rate(test_data, predictions_test)
+		print("Accuracy on test set : ",accuracy_test)
 
+def k_fold_cv_without_print(train_data, test_data):
+	k = 1
+	kfold = KFold(5, shuffle=True)
+	avg_accuracy = 0
+	avg_accuracy_train = 0
+	for train, valid in kfold.split(train_data):
+		i_fold_train = []
+		i_fold_valid = []
+		for i in train:
+			i_fold_train.append(train_data[i])
+		for i in valid:
+			i_fold_valid.append(train_data[i])
+		i_fold_train, i_fold_valid, test_data = normalize_data(i_fold_train, i_fold_valid, test_data)
+		info = MeanAndStdDevForClass(i_fold_train)
+		logBerProb = berProb(i_fold_train) 
+		predictions = getPredictions(info, logBerProb, i_fold_valid) 
+		accuracy = accuracy_rate(i_fold_valid, predictions) 
+		avg_accuracy += accuracy
+		k = k + 1
+	avg_accuracy /= 5
+	return avg_accuracy
 		
-# driver code 
-# add the data path in your system 
-filename = r'after_prepoc.csv'
-use_inbuilt_nb = False
+def seq_back_sel(train_data, test_data):
+	test_dup = copy.deepcopy(test_data)
+	original_avg_acc = k_fold_cv_without_print(train_data, test_dup)
+	initial_total = len(train_data[0])
+	print("Initial Features:",initial_total)
+	index_list = []
+	for i in range(0,len(train_data[0])):
+		index_list.append(i) 
+	print("Before Sequential Backward Selection accuracy : ",original_avg_acc)
+	print("Performing Sequential Backward Selection....")
+	print("=======This may take some time due to iteration on large number of features(199), number of times=======")
+	while(len(train_data[0])>1):
+		feature_index_to_delete = -1
+		max_acc = original_avg_acc
+		for ind in range(0,len(train_data[0])):
+			train_after_del = copy.deepcopy(train_data)
+			for row in train_after_del:
+				del row[ind]
+			del_acc = k_fold_cv_without_print(train_data=train_after_del, test_data=test_dup)
+			if del_acc>max_acc:
+				max_acc = del_acc
+				feature_index_to_delete = ind
+		if max_acc>original_avg_acc:
+			original_avg_acc = max_acc
+			print("Feature Index removed : ",feature_index_to_delete)
+			print("New Accuracy:",original_avg_acc)
+			del index_list[feature_index_to_delete]
+			for row in train_data:
+				del row[feature_index_to_delete]
+			for row in test_data:
+				del row[feature_index_to_delete]
+			for row in test_dup:
+				del row[feature_index_to_delete]
+		else:
+			break
+	print("Final accuracy after Sequential Backward Selection: ",original_avg_acc)
+	print("Number of features left:",len(train_data[0]),", Number of features removed:",initial_total-len(train_data[0]))
+	print("Index number of features left:",index_list)
+	k_fold_cv(train_data=train_data, test_data=test_data)
 
-# load the file and store it in mydata list 
-mydata = csv.reader(open(filename, "rt")) 
-mydata = list(mydata)
-mydata = encode_class(mydata) 
-for i in range(len(mydata)): 
-	mydata[i] = [float(x) for x in mydata[i]] 
-# mydata = normalize_data_2(mydata)
+def outlier_del(data):
+	print("Initializing sample removal....")
+	print("Initial sample size:",len(data))
+	to_return = []
+	feature_mean_std = MeanAndStdDev(data)
+	row_num = 0
+	zeros = 0
+	ones = 0
+	twos = 0
+	for sample in data:
+		row_num = row_num + 1
+		outliers = 0
+		for i in range(0,len(sample)-1):
+			m,v = feature_mean_std[i]
+			if(m+v*3 < sample[i]):
+				outliers = outliers + 1
+		if(outliers==0):
+			zeros = zeros+1
+			to_return.append(sample)
+		if(outliers==1):
+			ones = ones+1
+			to_return.append(sample)
+		if(outliers==2):
+			twos = twos+1
+		row_num = row_num + 1
+	print("Sample with Zero outlier:",zeros,"Sample with One outlier:",ones,"Sample with Twos outliers:",twos)
+	print("After sample removal training size:",len(to_return),", Samples removed:",len(data)-len(to_return))
+	return to_return
 
+def main() : 
+	filename = r'after_prepoc.csv'
 	
+	mydata = csv.reader(open(filename, "rt")) 
+	mydata = list(mydata)
+	mydata = encode_class(mydata) 
+	for i in range(len(mydata)): 
+		mydata[i] = [float(x) for x in mydata[i]] 
 
-k_fold_cv(mydata, pca=False, use_inbuilt_nb=use_inbuilt_nb)
-k_fold_cv(mydata, pca=True, use_inbuilt_nb=use_inbuilt_nb)
-exit()
+	ratio = 0.8
+	train_data, test_data = splitting(mydata, ratio) 
+		
+	print(" ========= Part 1 =========")
+	k_fold_cv(train_data=train_data , pca=False, use_inbuilt_nb=use_inbuilt_nb, test_data=test_data)
+	print("\n\n ========= Part 2 =========")
+	k_fold_cv(train_data=train_data, pca=True, use_inbuilt_nb=use_inbuilt_nb, test_data=test_data)	
+	print("\n\n ========= Part 3 =========")
+	after_samp_del = outlier_del(train_data)
+	seq_back_sel(after_samp_del,test_data)
+
+if __name__ == "__main__":
+	main()
